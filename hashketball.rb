b2086fbf5e2bc8891b6fd84b9e29f62acfc94a0e
michael hussain <smhussain_1@yahoo.com>
2019-01-06 22:51:49 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,17 @@ require "pry"
 
 def game_hash 
   game_hash = {
-    :home => {:team_name => :colors => :players => 
-    
+    :home => {:team_name => '', :colors => [], :players => {"" => 
+      :number
+      :shoe
+      :points
+      :rebounds
+      :assists
+      :steals
+      :blocks
+      :slam_dunks
+    } 
+    }
     
     :away => {:team_name => :colors => :players =>
   }
