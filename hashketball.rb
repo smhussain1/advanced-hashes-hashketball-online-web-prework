a7158c4d815ae7da2837da862c866bed426d4684
michael hussain <smhussain_1@yahoo.com>
2019-01-06 23:09:54 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -4,16 +4,41 @@ require "pry"
 def game_hash 
   game_hash = {
     :home => {:team_name => 'Brooklyn Nets', :colors => ["Black", "White"], :players => {"Alan Anderson" => 
-      {:number => 0 
-      :shoe => 16
-      :points => 22
+      {:number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12, 
+      :assists => 12, 
+      :steals => 3, 
+      :blocks => 1, 
+      :slam_dunks => 1},
+      "Reggie Evans" =>  
+      {:number => 30 
+      :shoe => 14
+      :points => 12
       :rebounds => 12 
       :assists => 12 
-      :steals => 3 
+      :steals => 12 
       :blocks => 1 
-      :slam_dunks => 1 
-    } 
-    }
+      :slam_dunks => 7}, 
+      "Brook Lopez" => 
+      {:number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounds => 19, 
+      :assists => 10, 
+      :steals => 3, 
+      :blocks => 1, 
+      :slam_dunks => 15},
+      "Mason Plumlee" => 
+      {:number => 11,
+      :shoe => 17,
+      :points => 17,
+      :rebounds => 19, 
+      :assists => 10, 
+      :steals => 3, 
+      :blocks => 1, 
+      :slam_dunks => 15},
     }
     
     :away => {:team_name => '', :colors => [], :players => {"" => 
